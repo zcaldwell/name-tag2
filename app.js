37bf8fd52e59e1765bf08b7c63ce11c30b1ff1df
Zachary Caldwell <mintyst.john@gmail.com>
2021-08-25 14:37:30 -0700

    working on color change

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-bad
+
+const nameElement = document.getElementById('name');
+
+const updateButton = document.getElementById('update-button');
+
+const nameInput = document.getElementById('name-input');
+
+updateButton.addEventListener('click', () => {
+
+    const name = nameInput.value;
+
+    nameElement.textContent = name;
+});
+
+const greenButton = document.getElementById('green-button');
+
+greenButton.addEventListener('Click', () => {
+   
+    const tag = tag.value;
+    
+    document.body.style.background = 'green';
+});
